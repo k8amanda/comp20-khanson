f0e5c73979f4fd722203b442d6e8e8fec9c96170
k8amanda <39542056+k8amanda@users.noreply.github.com>
2018-06-16 21:52:07 -0400

    code for lab.js

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,18 @@
-// Your JavaScript goes here...
+function parse()
+{
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.overrideMimeType("application/json");
+	xmlhttp.onreadystatechange = function()
+	{
+		if ((this.readyState == 4) && (this.status == 200))
+		{
+			var obj = JSON.parse(this.responseText);
+			for (x in obj)
+			{
+				document.getElementById("messages").innerHTML += obj[x].content + " - " + obj[x].username + "<br>";
+			}
+		}
+	};
+	xmlhttp.open("GET", "https://messagehub.herokuapp.com/messages.json", true);
+	xmlhttp.send();
+}
